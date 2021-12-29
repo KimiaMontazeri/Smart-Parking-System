0df876c25cf458f303a8546adf2e4a2317b24740
Kimia Montazeri <kimia.montazeri@yahoo.com>
2021-12-30 02:21:01 +0330

    added module 2: token production

--- module_2/token_production.v
+++ module_2/token_production.v
@@ -6,24 +6,27 @@
 --  *******************************************************
 --  All Rights reserved (C) 2021-2022
 --  *******************************************************
---  Student ID  : 
---  Student Name: 
---  Student Mail: 
+--  Student ID  : 9931078
+--  Student Name: Kimia Montazeri
+--  Student Mail: kimia.mtz@gmail.com 
 --  *******************************************************
---  Additional Comments:
---
+--  Additional Comments: This module is for encrypting the park number using a certain pattern.
+--  We have used XOR operation for this purpose; and the reason behind it is that if you don't know 
+--  the XOR-encryption key before decrypting the encrypted data, it is impossible to decrypt that data.
 --*/
 
 /*-----------------------------------------------------------
 ---  Module Name: token_production 
 -----------------------------------------------------------*/
 `timescale 1 ns/1 ns
-module token_production(
- park_number,
- pattern,
- token);
-input [2:0] park_number;
-input [2:0] pattern;
-output [2:0] token;
- // write your code here, please.
+module token_production(park_number, pattern, token);
+
+    input  [2:0] park_number;
+    input  [2:0] pattern; // constant number: 010
+    output [2:0] token;
+
+    assign token[0] = park_number[0] ^ pattern[0];
+    assign token[1] = park_number[1] ^ pattern[1];
+    assign token[2] = park_number[2] ^ pattern[2];
+
 endmodule
