ea058926dc7e80ff0f63a80ee234a91041c00339
sina shariati <sina.shariati@yahoo.com>
2022-01-03 18:14:53 +0330

    Finish half_subtractor.v

--- module_5/subtractors/half_subtractor.v
+++ module_5/subtractors/half_subtractor.v
@@ -1,11 +1,14 @@
 `timescale 1 ns/1 ns
 
-module half_subtractor(bit1, bit2, diff, carry);
+module half_subtractor(bit1, bit2, diff, borrow);
 input bit1;
 input bit2;
 output diff;
-output carry;
+output borrow;
+
+wire bit1_not;
+not bit_not_calculator(bit1_not, bit1);
 
 xor diff_calculator(diff, bit1, bit2);
-and carry_calculator(carry, bit1, bit2);
+and borrow_calculator(borrow, bit1_not, bit2);
 endmodule
