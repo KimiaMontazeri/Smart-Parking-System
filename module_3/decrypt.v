eec4efc39e1202153ed5141de61289247b4bd43e
Kimia Montazeri <kimia.montazeri@yahoo.com>
2022-01-06 18:26:38 +0330

    fixed latch warning

--- module_3/decrypt.v
+++ module_3/decrypt.v
@@ -20,15 +20,10 @@
 `timescale 1 ns/1 ns
 module decrypt(exit, token, pattern, park_number);
     input exit;
-    input [2:0] token;
-    input [2:0] pattern;
-    output reg [2:0] park_number;
+    input  [2:0] token;
+    input  [2:0] pattern;
+    output [2:0] park_number;
     
-    always @ (exit or token or pattern)
-        if (exit) begin
-            park_number[0] = token[0] ^ pattern[0];
-            park_number[1] = token[1] ^ pattern[1];
-            park_number[2] = token[2] ^ pattern[2];
-        end 
+    assign park_number = exit ? (token ^ pattern) : 3'bxxx; 
     
 endmodule
