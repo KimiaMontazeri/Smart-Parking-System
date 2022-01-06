dad0365dcdfb570daa0996ec526a2a59902a4a91
Kimia Montazeri <kimia.montazeri@yahoo.com>
2022-01-06 14:57:44 +0330

    added more cases to testbench

--- module_3/calculate_new_capacity_tb.v
+++ module_3/calculate_new_capacity_tb.v
@@ -12,14 +12,16 @@ module calculate_new_capacity_tb;
         $dumpfile("result.vcd");
         $dumpvars(0, calculate_new_capacity_tb);
 
+        // car exits
         park_location = 8'b00000001; parking_capacity = 8'b11000000; #20;
         park_location = 8'b00000010; parking_capacity = 8'b11000001; #20;
         park_location = 8'b00000100; parking_capacity = 8'b11000000; #20;
         park_location = 8'b00001000; parking_capacity = 8'b11000011; #20;
-        park_location = 8'b00010000; parking_capacity = 8'b00100010; #20;
-        park_location = 8'b00100000; parking_capacity = 8'b11000110; #20;
-        park_location = 8'b01000000; parking_capacity = 8'b10000111; #20;
-        park_location = 8'b10000000; parking_capacity = 8'b01100100; #20;
+        // car enters
+        park_location = 8'b00010000; parking_capacity = 8'b00110010; #20;
+        park_location = 8'b00100000; parking_capacity = 8'b11100110; #20;
+        park_location = 8'b01000000; parking_capacity = 8'b11000111; #20;
+        park_location = 8'b10000000; parking_capacity = 8'b11100100; #20;
 
         $finish;
     end 
