85d90935e331ceef9c6efd9a8056fcc671ba99d7
Kimia Montazeri <kimia.montazeri@yahoo.com>
2022-01-22 10:04:59 +0330

    smart_parking completed

--- module_6/smart_parking.v
+++ module_6/smart_parking.v
@@ -17,6 +17,13 @@
 /*-----------------------------------------------------------
 ---  Module Name: smart_parking
 -----------------------------------------------------------*/
+`include "update_capacity.v"
+`include "../module_1/entry_park.v"
+`include "../module_2/token_production.v"
+`include "../module_3/exit_park.v"
+`include "../module_3/calculate_new_capacity.v"
+`include "../module_4/time_calculate.v"
+`include "../module_5/parking_capacity_counter.v"
 `timescale 1 ns/1 ns
 module smart_parking(
 input entry,
@@ -26,10 +33,22 @@ input [2:0] pattern,
 input [7:0] time_out,
 input [7:0] time_in,
 output [7:0] new_capacity,
-output [7:0] time_total;
-output [3:0] parked;
-output [3:0] empty;
+output [7:0] time_total,
+output [3:0] parked,
+output [3:0] empty
 );
 
- // write your code here, please.
+    wire [2:0] park_number;
+    wire [2:0] token;
+    wire [7:0] park_location;
+    wire [7:0] parking_capacity_new;
+    wire [7:0] cap;
+
+    entry_park has_entered(entry, parking_capacity, park_number);
+    token_production get_token(park_number, pattern, token);
+    exit_park has_exited(exit, token, pattern, park_location);
+    update_capacity update_cap(entry, parking_capacity, parking_capacity_new, cap);
+    calculate_new_capacity get_new_cap(park_location, parking_capacity_new, new_capacity);
+    parking_capacity_counter get_parking_cap(new_capacity, parked, empty);
+    time_calculate cal_time(time_out, time_in, time_total);
 endmodule
